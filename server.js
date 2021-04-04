23caec1698db4ee11ef84367b418bfb0a14bf932
Miguel Velazquez <41972914+mavo97@users.noreply.github.com>
2021-04-04 15:33:13 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const app = express();
+const port = 4000;
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
+app.listen({ port }, () =>
+  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`),
+);
