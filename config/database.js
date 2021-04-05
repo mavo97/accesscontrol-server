57ae8a7b1d8ae427914541792ffa01c7919fc65e
mavo97 <mavo6939@gmail.com>
2021-04-05 12:29:41 -0500

    Changes on database.js

--- config/database.js
+++ config/database.js
@@ -4,23 +4,20 @@ dotenv.config();
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
+  port: 5432,
   dialect: 'postgres',
-
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
+  ssl: {
+    rejectUnauthorized: false, // <<<<<<< YOU NEED THIS
   },
 });
 
-async function testConnection() {
-  try {
-    await db.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-}
+// async function testConnection() {
+//   try {
+//     await db.authenticate();
+//     console.log('Connection has been established successfully.');
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
+// }
 
 module.exports = db;
