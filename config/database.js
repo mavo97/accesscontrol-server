8adaea7bbc3057594421fc0bb7fad2c786393bd7
mavo97 <mavo6939@gmail.com>
2021-04-05 00:39:10 -0500

    changes

--- config/database.js
+++ config/database.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('accesscontrol', 'postgres', '02089712', {
-  host: 'localhost',
+const dotenv = require('dotenv');
+dotenv.config();
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
   dialect: 'postgres',
 
   pool: {
