62fc6b1b77f0caf06511d6603ffeb9fff2d89408
mavo97 <mavo6939@gmail.com>
2021-04-05 12:15:27 -0500

    server.js to index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const typeDefs = require('./schema');
+const resolvers = require('./resolver');
+const db = require('./config/database');
+
+const app = express();
+const port = 4000;
+
+const server = new ApolloServer({ typeDefs, resolvers, context: { db } });
+server.applyMiddleware({ app });
+db.authenticate();
+db.sync();
+app.listen({ port }, () =>
+  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`),
+);
